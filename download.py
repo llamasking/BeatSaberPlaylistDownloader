dd09ce2d396e95de08faa0702a2274f3608daee7
llamasking <21324788+llamasking@users.noreply.github.com>
2020-06-22 23:34:02 -0700

    Rewrite the entire fucking thing in Python...
    because NodeJS was never built to do the task and I really didn't want to write it in Python then.
    I still don't like Python but holy hell is it so much better at the task than Node ever could be.
    The entire rewrite was a breeze compared to the living hell that Node was.

---
+++ download.py
@@ -0,0 +1,99 @@
+# Import things that will be needed.
+import configparser
+import urllib.request, urllib.parse
+import json
+from pathlib import Path
+
+# Parse config.ini.
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Create download path if it doesn't exist.
+downloadPath = Path(config['Download']['Download Path'])
+if not downloadPath.is_dir(): Path.mkdir(downloadPath)
+
+# Setup Beat Saber Playlist.
+bspl = {
+    "playlistTitle": config['Playlist']['Name'],
+    "playlistAuthor": "BeatSaverPlaylistDownloader",
+    "image": '',
+    "songs": []
+}
+
+# Parse download list.
+downloadList = open(config['Download']['Playlist File']).read().strip().splitlines()
+
+# Function to convert a string to a bool. I just wanna stay in base Python.
+def strtobool(input): return input.lower() in ['y', 'yes', 't', 'true', 'on', '1']
+
+# Function to verify map has difficulties required.
+def checkDifficulty(mapDiffs):
+    mapDiffs = mapDiffs['metadata']['difficulties']
+    allDiffs = True
+    for diff in mapDiffs:
+        if strtobool(config['Difficulties'][diff]):
+            if strtobool(config['Difficulties'][diff]) != mapDiffs[diff]: allDiffs = False
+    return allDiffs
+
+# Function to check if map is downloaded and if not, download it.
+def downloadMap(map, dlMode):
+    downloadPath = Path(config['Download']['Download Path'] + map['hash'] + '.zip')
+    if downloadPath.is_file():
+        print(dlMode + ' [Exists] Skipping Map: ' + map['name'])
+    else:
+        print(dlMode + ' Downloading Map: ' + map['name'])
+
+        # Add http header to get around whatever is spitting a forbidden page.
+        req = urllib.request.Request('https://beatsaver.com' + map['directDownload'])
+        req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0')
+
+        # Download map
+        with urllib.request.urlopen(req) as dl:
+            downloadPath.write_bytes(dl.read())
+
+        # Add map to playlist file if enabled.
+        if strtobool(config['Playlist']['Generate']):
+            bspl['songs'].append({"hash": map['hash']})
+            Path(config['Download']['Download Path'] + config['Playlist']['Name'] + '.bplist').write_text(json.dumps(bspl))
+
+# Loop through each song in the download list.
+for songName in downloadList:
+    print('\nSearching for Song: ' + songName)
+
+    # Add http header to get around whatever is spitting a forbidden page.
+    req = urllib.request.Request('https://beatsaver.com/api/search/text?q=' + urllib.parse.quote(songName))
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0')
+
+    # Request the search results for the song's name and parse the json.
+    with urllib.request.urlopen(req) as url:
+        searchResults = json.loads(url.read().decode())['docs']
+
+        # Download Modes
+        if strtobool(config['Mode']['First Result']):
+            if checkDifficulty(searchResults[0]):
+                downloadMap(searchResults[0], '[FR]')
+            else:
+                print('[FR] [Diff] Skipping Map: ' + searchResults[0]['name'])
+
+        if strtobool(config['Mode']['Most Downloads']):
+            # Just set the map to download to the first so there's something to compare against.
+            mapToDL = searchResults[0]
+            for map in searchResults:
+                if map['stats']['downloads'] > mapToDL['stats']['downloads']: mapToDL = map
+
+            if checkDifficulty(mapToDL):
+                downloadMap(mapToDL, '[DL]')
+            else:
+                print('[DL] [Diff] Skipping Map: ' + mapToDL['name'])
+
+        if strtobool(config['Mode']['Most Upvotes']):
+            # Just set the map to download to the first so there's something to compare against.
+            mapToDL = searchResults[0]
+            for map in searchResults:
+                if map['stats']['upVotes'] > mapToDL['stats']['upVotes']: mapToDL = map
+
+            if checkDifficulty(mapToDL):
+                downloadMap(mapToDL, '[UV]')
+            else:
+                print('[UV] [Diff] Skipping Map: ' + mapToDL['name'])
+
